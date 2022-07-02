77426fbe7e40413b3bfe301cd089a8a01fd0e2bc
symphony <jciurc@gmail.com>
2022-07-01 19:59:09 -0600

    rename vars to be more verbose. add description for helper function

--- caesar.js
+++ caesar.js
@@ -1,16 +1,17 @@
-const mod = (n, m, start = 0) => {
+/** keeps number within positive range (with optional starting position) */
+const modulo = (n, m, start = 0) => {
   return (n - start + m) % m + start;
 };
 
 const shiftChar = (char, amount = 0) => {
   const range = 26; // alphabet length
   const ansiOffset = 97; // small letters start at 97
-  const shifted = mod(char.charCodeAt() + amount, range, ansiOffset);
+  const shifted = modulo(char.charCodeAt() + amount, range, ansiOffset);
   return String.fromCharCode(shifted);
 };
 
 const encrypt = (plaintext, key) => {
-  return plaintext.split('').map((c) => c === ' ' ? c : shiftChar(c, key)).join('');
+  return plaintext.split('').map((char) => char === ' ' ? char : shiftChar(char, key)).join('');
 };
 
 export default encrypt;
